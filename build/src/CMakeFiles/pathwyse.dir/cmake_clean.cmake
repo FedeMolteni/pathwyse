cb6629584104a365873e04584b7f96a312e22c6e
Federico Molteni <federicomolteni@MacBook-Pro-di-Federico-2.local>
2025-11-20 08:53:59 +0100

    Initialization after install

---
+++ build/src/CMakeFiles/pathwyse.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/bin/pathwyse"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/bin/pathwyse.pdb"
+  "CMakeFiles/pathwyse.dir/standalone/main.cpp.o"
+  "CMakeFiles/pathwyse.dir/standalone/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pathwyse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/pathwyse_core.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/algorithms/algorithm.cpp" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/algorithm.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/algorithm.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/algorithms/dynamic_programming/PW_acyclic/LM_acyclic.cpp" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/dynamic_programming/PW_acyclic/LM_acyclic.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/dynamic_programming/PW_acyclic/LM_acyclic.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/algorithms/dynamic_programming/PW_acyclic/PW_acyclic.cpp" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/dynamic_programming/PW_acyclic/PW_acyclic.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/dynamic_programming/PW_acyclic/PW_acyclic.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/algorithms/dynamic_programming/PW_default/LM_default.cpp" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/dynamic_programming/PW_default/LM_default.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/dynamic_programming/PW_default/LM_default.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/algorithms/dynamic_programming/PW_default/PW_default.cpp" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/dynamic_programming/PW_default/PW_default.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/dynamic_programming/PW_default/PW_default.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/algorithms/labels/label.cpp" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/labels/label.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/labels/label.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/algorithms/labels/label_advanced.cpp" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/labels/label_advanced.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/labels/label_advanced.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/algorithms/preprocessing/dijkstra.cpp" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/preprocessing/dijkstra.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/preprocessing/dijkstra.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/algorithms/preprocessing/preprocessing.cpp" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/preprocessing/preprocessing.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/algorithms/preprocessing/preprocessing.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/data/graph.cpp" "src/CMakeFiles/pathwyse_core.dir/core/data/graph.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/data/graph.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/data/path.cpp" "src/CMakeFiles/pathwyse_core.dir/core/data/path.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/data/path.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/data/problem.cpp" "src/CMakeFiles/pathwyse_core.dir/core/data/problem.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/data/problem.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/resources/capacity.cpp" "src/CMakeFiles/pathwyse_core.dir/core/resources/capacity.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/resources/capacity.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/resources/defaultcost.cpp" "src/CMakeFiles/pathwyse_core.dir/core/resources/defaultcost.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/resources/defaultcost.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/resources/node_limit.cpp" "src/CMakeFiles/pathwyse_core.dir/core/resources/node_limit.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/resources/node_limit.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/resources/time.cpp" "src/CMakeFiles/pathwyse_core.dir/core/resources/time.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/resources/time.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/resources/time_windows.cpp" "src/CMakeFiles/pathwyse_core.dir/core/resources/time_windows.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/resources/time_windows.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/solver.cpp" "src/CMakeFiles/pathwyse_core.dir/core/solver.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/solver.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/utils/bitset.cpp" "src/CMakeFiles/pathwyse_core.dir/core/utils/bitset.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/utils/bitset.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/utils/data_collector.cpp" "src/CMakeFiles/pathwyse_core.dir/core/utils/data_collector.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/utils/data_collector.cpp.o.d"
+  "/Users/federicomolteni/Desktop/UNI/terzo anno/Algoritmi Avanzati/ProgettoLabo/pathwyse/src/core/utils/param.cpp" "src/CMakeFiles/pathwyse_core.dir/core/utils/param.cpp.o" "gcc" "src/CMakeFiles/pathwyse_core.dir/core/utils/param.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
